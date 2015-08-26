62c851f2e13aba44e8d959b999c21cc864a45061
OleksandrGorobets <oleksandr.gorobets@gmail.com>
2015-08-26 16:16:53 +0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        auto_install: {
+            subdir: {
+                options: {
+                    cwd: 'node_modules/express/',
+                    stdout: true,
+                    stderr: true,
+                    failOnError: true,
+                }
+            },
+            subdir_2: {
+                options: {
+                    cwd: 'node_modules/express/public/',
+                    stdout: true,
+                    stderr: true,
+                    failOnError: true,
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-auto-install');
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Prerequisites:
+1. MongoDB
+
+Intallation of the app:
+1. Go to project root folder
+2. Enter 'grunt auto_intall' in command line
+
+Running app:
+1. Run MongoDB
+(being in MongoDB 'bin' folder enter in command line 'mongod --dbpath <path_to_project_folder\database>')
+2. Go to node_modules\express folder
+3. Enter 'node index.js' in command line
